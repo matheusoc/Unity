cab0dd5b76cfd366f12a3ef6b4c1e0912f33ab3b
Matheus de Oliveira Campos <matheuscampos@gec.inatel.br>
2017-09-24 23:09:25 -0300

    fase um pronta

---
+++ Link's Adventure/Assets/scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	//Posição do player.
+	public Transform player;
+
+	//Velocidade do movimento da camera.
+	private Vector2 velocity;
+
+	//Suavização do movimeno da camera.
+	public float smooth;
+
+	// Use this for initialization
+	void Start () {
+		velocity = new Vector2 (0.5f, 0.5f);
+		smooth = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Calcula nova posição da camera com base no player.
+
+		Vector3 newPosition = Vector3.zero;
+
+		newPosition.x = Mathf.SmoothDamp (transform.position.x, player.position.x, 
+			ref velocity.x, smooth);
+		newPosition.y = Mathf.SmoothDamp (transform.position.y, player.position.y,
+			ref velocity.y, smooth);
+
+		newPosition.z = transform.position.z;
+
+		transform.position = Vector3.Slerp (transform.position, newPosition, Time.time);
+	}
+}
--- Link's Adventure/Assets/scripts/PlayerController.cs
+++ Link's Adventure/Assets/scripts/PlayerController.cs
@@ -4,13 +4,71 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+	//Variavel responsavel por referenciar o Animator Controller.
+	public Animator animator;
+
+	//Intensidade do pulo.
+	public int jumpForce;
+
+	public int velocity;
+
+	//Transform contendo a posição do objeto groundCheck.
+	public Transform groundCheck;
+
+	//Camada do chão.
+	public LayerMask isFloor;
+
+	//Instancia do Rigidbody do personagem.
+	public Rigidbody2D body;
+
+	//Variavel que indica se o player está no chão.
+	public bool inFloor;
+
+	// Use para inicialização.
 	void Start () {
-		
+
 	}
-	
-	// Update is called once per frame
+
+	// Chamado cada vez que o frame é atualizado.
 	void Update () {
-		
+
+		//Recebe o estado do botão Run.
+		bool run = Input.GetButton ("Run");
+
+		//Recebe o estado do botão Fire
+		bool attack = Input.GetButton ("Attack");
+
+		bool jump = Input.GetButtonDown ("Jump");
+
+		//Cria uma região para verificação, de raio 0.2, situada na mesma posição do objeto
+		//groundCheck, verificando a camada isFloor.
+		//Salva true caso esteja no chão, e false caso esteja no ar.
+		inFloor = Physics2D.OverlapCircle (groundCheck.position, 0.1f, isFloor);
+
+		if (Input.GetAxisRaw ("Horizontal") > 0) {
+			transform.Translate (Vector2.right*velocity*Time.deltaTime);
+			transform.eulerAngles = new Vector2 (0, 0);
+			run = true;
+		} else if (Input.GetAxisRaw ("Horizontal") < 0) {
+			transform.Translate (Vector2.right*velocity*Time.deltaTime);
+			transform.eulerAngles = new Vector2 (0, 180);
+			run = true;
+		}
+
+		//Verificação se o player está no chão e pressionou o botão jump.
+		if (inFloor & jump) {
+			body.AddForce (new Vector2(0, jumpForce));
+		}
+
+		//Passa o estado do botão run para a Animation Controller, para o parametro run.
+		animator.SetBool ("run", run);
+
+		//Passa o estado do botão jump para a Animation Controller, para o parametro jump.
+		animator.SetBool ("jump", !inFloor);
+
+		//Passa o estado do botão fire para a Animation Controller, para o parametro fire.
+		animator.SetBool ("attack", attack);
+
 	}
+
 }
