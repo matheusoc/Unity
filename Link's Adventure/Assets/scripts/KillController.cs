67f94b82d000c061909adfbc4fc3fd89b54f2416
Aluno do Inatel <aluno@local.inatel.br>
2017-10-05 20:35:10 -0300

    Colisions added

---
+++ Link's Adventure/Assets/scripts/KillController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillController : MonoBehaviour {
+
+	public Transform groundCheck;
+
+	public LayerMask spikes;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Physics2D.OverlapCircle (groundCheck.position, 0.05f, spikes)) {
+			Application.LoadLevel ("gameover");
+		}
+
+	}
+}
